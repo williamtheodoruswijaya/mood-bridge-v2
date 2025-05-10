ff7ef11fb3f4cd77d8c4720a8d717a057e7f3cc9
Admantix <williamwijaya499@gmail.com>
2025-05-10 21:10:25 +0700

    Add authentication middleware with JWT token validation and error handling

--- server/internal/middleware/auth_middleware.go
+++ server/internal/middleware/auth_middleware.go
@@ -1,1 +1,99 @@
 package middleware
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"mood-bridge-v2/server/internal/model/response"
+	"os"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/joho/godotenv"
+)
+
+type Claims struct {
+	User *response.CreateUserResponse `json:"user"`
+	jwt.RegisteredClaims
+}
+
+func Authenticate() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		token := c.GetHeader("authorization")
+		if token == "" {
+			c.JSON(401, gin.H{
+				"code":    401,
+				"message": "Unauthorized",
+			})
+			c.Abort()
+			return
+		}
+
+		err := ValidateToken(c, token)
+		if err != nil {
+			c.JSON(401, gin.H{
+				"code":    401,
+				"message": "Unauthorized",
+			})
+			c.Abort()
+			return
+		}
+
+		// lanjutkan ke handler berikutnya
+		c.Next()
+	}
+}
+
+func ValidateToken(c *gin.Context, token string) error {
+	// Split token
+	if !strings.Contains(token, "Bearer") {
+		return nil
+	}
+
+	// Extract token string
+	tokenString := strings.Split(token, " ")[1]
+	if tokenString == "" {
+		c.JSON(401, gin.H{
+			"code":    401,
+			"message": "Unauthorized",
+		})
+		c.Abort()
+		return nil
+	}
+
+	// Load .env file
+	err := godotenv.Load("../.env")
+	if err != nil {
+		err = godotenv.Load()
+		if err != nil {
+			log.Println("env not found, skipping...")
+		}
+	}
+	
+	// Parse token
+	claims := &Claims{}
+	jwtToken, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		jwtSecret := []byte(os.Getenv("jwt_secret_key"))
+		return jwtSecret, nil
+	})
+
+	if err != nil || !jwtToken.Valid {
+		c.JSON(401, gin.H{
+			"code":    401,
+			"message": "Unauthorized",
+		})
+		c.Abort()
+		return nil
+	}
+
+	// Set user data to context
+	userLogin := c.Request.WithContext(context.WithValue(c.Request.Context(), "user_login", claims.User))
+	c.Request = userLogin
+	c.Set("token", token)
+
+	return nil
+}
--- server/internal/middleware/middleware.go
+++ server/internal/middleware/middleware.go
@@ -1,1 +1,31 @@
 package middleware
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+// Middleware buat menangani panic (error yang tidak terduga) di server
+// Ini akan mengembalikan response JSON dengan status 500 Internal Server Error
+func HandlePanic() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// rollback function yang akan dieksekusi
+		defer func() {
+			r := recover()
+			if r != nil {
+				log.Println("Panic occurred:", r)
+				c.JSON(http.StatusInternalServerError, gin.H{
+					"status": "error",
+					"message": "Internal server error",
+				})
+				c.Abort()
+			}
+		}()
+
+		// lanjutkan ke handler berikutnya
+		// Jika ada panic di handler berikutnya, maka defer function di atas akan dieksekusi
+		c.Next()
+	}
+}
