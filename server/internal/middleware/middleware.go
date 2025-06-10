aa0c7c16909a4ef76fb3bdeeeb22ef1f64823a2d
William Theodorus <williamwijaya499@gmail.com>
2025-06-10 11:28:54 +0700

    feat: enhance CORS middleware to allow dynamic origin handling (#27)

--- server/internal/middleware/middleware.go
+++ server/internal/middleware/middleware.go
@@ -33,7 +33,10 @@ func HandlePanic() gin.HandlerFunc {
 // Intinya biar localhost:8080 bisa diakses sama localhost:3000
 func CORSMiddleware() gin.HandlerFunc {
     return func(c *gin.Context) {
-        c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		origin := c.Request.Header.Get("Origin")
+		if origin == "http://localhost:3000" || origin == "https://mood-bridge-v2.vercel.app" {
+            c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
+        }
         c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
         c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
         c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
